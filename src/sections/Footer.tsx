abc63e61d493d3d29510eccdff7eef6b07ca0c7a
wilfredmanyara <manyarawilly46@gmail.com>
2024-12-01 11:12:48 +0300

    add footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,6 @@
+import logoImage from '@/assets/images/logo.svg'
+import Image from 'next/image';
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -5,5 +8,23 @@ const footerLinks = [
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+    return (
+        <section className='py-16'>
+            <div className="container">
+                <div className='flex flex-col md:flex-row items-center md:justify-between gap-6'>
+                    <div>
+                        <Image src={logoImage} alt="Layers Logo" />
+                    </div>
+                    <div>
+                        <nav className='flex gap-6'>
+                            {footerLinks.map((link) => (
+                                // eslint-disable-next-line react/jsx-key
+                                <a href={link.href} className='text-white/50 text-sm'>{link.label}</a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
 }
